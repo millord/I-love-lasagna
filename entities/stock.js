8720cf64834c26e4d0d7bd97c2b6500470f0468d
jmillord <juan.millord@contantcontact.com>
2022-03-20 17:58:39 -0400

    added the supplier and stock entities

---
+++ entities/stock.js
@@ -0,0 +1,8 @@
+export default class Stock {
+  id; // integer
+  supplierId; // integer
+  dishId; // integer
+  itemsPurchased; // integer
+  description; // string
+  purchaseDate; // date
+}
---
+++ entities/supplier.js
@@ -0,0 +1,7 @@
+export default class Supplier {
+  id; // integer
+  name; // string
+  email; // string
+  phone; // string
+  description; // string
+}
