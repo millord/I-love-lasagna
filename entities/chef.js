3a8efe636cd69233546506481af025dcab4eb705
jmillord <juan.millord@contantcontact.com>
2022-03-20 17:52:16 -0400

    added the chef and customer entities

---
+++ entities/chef.js
@@ -0,0 +1,7 @@
+export default class Chef {
+  id; // integer
+  name; // string
+  experience; // enum
+  skills; // array
+  address; // string
+}
---
+++ entities/customer.js
@@ -0,0 +1,7 @@
+export default class Customer {
+  id; // integer
+  name; // string
+  email; // string
+  phone; // string
+  address; // string
+}
