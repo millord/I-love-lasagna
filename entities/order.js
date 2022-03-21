ea6807a31fce86f5abe3d0ff655cedb91d9f510c
juanmillord <juancarlosmillord@gmail.com>
2022-03-21 12:46:28 -0400

    adding order and order status entities (#3)
    * adding order and order status entities
    
    Co-authored-by: jmillord <juan.millord@contantcontact.com>

---
+++ entities/order.js
@@ -0,0 +1,10 @@
+export default class Order {
+  id; // integer
+  customerId; // integer
+  orderStatusId; // integer
+  dishList; // array
+  total; // integer
+  orderDate; // date
+  chefId; // integer
+  difficulty; // enum
+}
