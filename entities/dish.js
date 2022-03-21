06303eec36dc64ec423ba55be77151f4fa4bee2f
juanmillord <juancarlosmillord@gmail.com>
2022-03-21 14:03:36 -0400

    adding dish and dish category entities (#4)
    * adding dish and dish category entities
    
    Co-authored-by: jmillord <juan.millord@contantcontact.com>

---
+++ entities/dish.js
@@ -0,0 +1,7 @@
+export default class Dish {
+  id; // integer
+  dishCategoryId; // integer
+  name; // string
+  description; // string
+  price; // integer
+}
---
+++ entities/dishCategory.js
@@ -0,0 +1,4 @@
+export default class DishCategory {
+  id; // integer
+  category; // string
+}
